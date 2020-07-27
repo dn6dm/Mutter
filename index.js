afeb5cd491ebcb1640c8248e58212f2a545d0f86
Dan Nguyen <dn6dm@virginia.edu>
2020-07-27 17:37:45 -0400

    Set up for AWS deployment

--- index.js
+++ index.js
@@ -40,6 +40,12 @@ app.use((err, req, res, next) => {
   next();
 });
 
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
